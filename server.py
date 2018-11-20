cfe5d71ff28a0c4284a5c834ecf23dadc2ef2b71
Tom Dobra <tom.dobra@btinternet.com>
2018-11-20 23:16:51 +0000

    Reads all resource files and calls LaTeX compiler, but won't compile

--- server.py
+++ server.py
@@ -6,25 +6,42 @@ import webbrowser
 import threading
 
 PORT = 8000
+MAXPORT = 8099  # Maximum port number to try
 
 # Start server
 Handler = http.server.SimpleHTTPRequestHandler
-httpd = socketserver.TCPServer(("", PORT), Handler)
-server_thread = threading.Thread(target=httpd.serve_forever)
-# Exit the server thread when the main thread terminates
-server_thread.daemon = True
-server_thread.start()
+portok = False
+while portok == False:
+    try:
+        httpd = socketserver.TCPServer(("", PORT), Handler)
+    except OSError as err:
+        if err.errno == 48:
+            # Socket in use
+            if PORT >= MAXPORT:
+                print("ERROR: Could not find a port for server.")
+                break
+                
+            PORT = PORT + 1
 
-# Display status
-urlstr = "http://localhost:" + str(PORT)
-print("Open " + urlstr + " in your web browser.")
-print("Press Enter to exit.")
-webbrowser.open(urlstr)
+    else:
+        portok = True
 
-# Wait for user
-input()
 
-# Exit
-httpd.shutdown()
-httpd.server_close()
-print("Program closed. You may need to wait a minute before reopening.")
+if portok == True:
+    server_thread = threading.Thread(target=httpd.serve_forever)
+    # Exit the server thread when the main thread terminates
+    server_thread.daemon = True
+    server_thread.start()
+
+    # Display status
+    urlstr = "http://localhost:" + str(PORT)
+    print("Open " + urlstr + " in your web browser.")
+    print("Press Enter to exit.")
+    webbrowser.open(urlstr)
+
+    # Wait for user
+    input()
+
+    # Exit
+    httpd.shutdown()
+    httpd.server_close()
